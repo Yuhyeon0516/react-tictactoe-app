98143922b4b326e0910a2e6419711f266dd15941
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-23 19:03:31 +0900

    클래스형 -> 함수형 Component로 변경 및 jumpTo 함수 작성

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "./Square.css";
 
-export default class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button
-        className="squara"
-        onClick={() => {
-          this.setState({ value: "X" });
-        }}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
-}
+const Square = ({ onClick, value }) => {
+  return (
+    <button
+      className="squara"
+      onClick={() => {
+        onClick();
+      }}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
